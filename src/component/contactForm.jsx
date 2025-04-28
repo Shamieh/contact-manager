ff32c1305581ba734ac2aaf696b88af067537384
Shamieh <m7md2003sha100@gmail.com>
2025-04-28 14:00:23 +0300

    feat: contact form component added

---
+++ src/component/contactForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import './css/contactForm.css'
+
+
+const ContactForm = (addcontact, contacts) => {
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+
+
+    const addContactHandler = (e)=> {
+    
+        e.preventDefault();
+        const contactInfo ={
+            username,
+            email
+        };
+        addcontact(contactInfo);
+
+    }
+    return (
+        <div>
+            <form onSubmit={addContactHandler}>
+                <input 
+                type="text"
+                placeholder="Name"
+                value={username}
+                onChange={(e)=>setUsername(e.target.value)}
+                />
+                <input 
+                type="email"
+                placeholder="Email" 
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default ContactForm;
